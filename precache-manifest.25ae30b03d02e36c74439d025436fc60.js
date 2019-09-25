2bf3d9814cc7296a1ea5830699f712fc55da4c60
kimsanghoon1 <sanghoon01@uengine.org>
2019-09-25 22:17:12 +0900

    Initial dist subtree commit

---
+++ precache-manifest.25ae30b03d02e36c74439d025436fc60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "31ecd56f369cbaa93dc4",
+    "url": "/js/chunk-vendors.49f98882.js"
+  },
+  {
+    "revision": "607cf623d0b1df08c727",
+    "url": "/js/app.a2e4f856.js"
+  },
+  {
+    "revision": "e6ef8090b8eb031c01fa",
+    "url": "/js/about.568edb80.js"
+  },
+  {
+    "revision": "6a44a02c278f2382e936b0a6ac863c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31ecd56f369cbaa93dc4",
+    "url": "/css/chunk-vendors.73e8e3ae.css"
+  },
+  {
+    "revision": "607cf623d0b1df08c727",
+    "url": "/css/app.e3d154fc.css"
+  }
+];
